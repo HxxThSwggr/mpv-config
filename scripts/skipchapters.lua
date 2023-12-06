47cdd937452db661d6fda256a0b21ca01adfb621
HxxThSwggr <67126208+HxxThSwggr@users.noreply.github.com>
2023-12-06 02:34:39 +0100

    Add files via upload

---
+++ scripts/skipchapters.lua
@@ -0,0 +1,25 @@
+require 'mp.options'
+local opt = {
+    patterns_off = {
+        "OP","[Oo]pening$", "^[Oo]pening:", "[Oo]pening [Cc]redits",
+        "ED","[Ee]nding$", "^[Ee]nding:", "[Ee]nding [Cc]redits",
+        "[Pp]review$",
+    },
+    patters = {},
+}
+read_options(opt)
+
+function check_chapter(_, chapter)
+    if not chapter then
+        return
+    end
+    for _, p in pairs(opt.patterns) do
+        if string.match(chapter, p) then
+            print("Skipping chapter:", chapter)
+            mp.command("no-osd add chapter 1")
+            return
+        end
+    end
+end
+
+mp.observe_property("chapter-metadata/by-key/title", "string", check_chapter)
